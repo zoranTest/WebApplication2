1abf166b816bc6c34038c7a4e15e3bdea7d4b6da
Zoran <sourcecode3010@gmail.com>
2020-10-28 23:31:54 +0100

    dodatAbout_Kontroler

---
+++ WebApplication2/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+// na trecoj grani napravljen
+
+namespace WebApplication2.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
