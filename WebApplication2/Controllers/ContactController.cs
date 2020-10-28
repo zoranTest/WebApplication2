6ca09d0a486246a31ec353fcdb23f26d8d55164a
Zoran <sourcecode3010@gmail.com>
2020-10-29 00:02:36 +0100

    iz stasha contact controller

---
+++ WebApplication2/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ind()
+        {
+            return View();
+        }
+    }
+}
